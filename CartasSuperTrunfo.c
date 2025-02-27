9f7d3091a12310b8ce4bbd79247da43ac02f4079
EduardosilvaXx <eduardo_chexo@hotmail.com>
2025-02-27 23:54:44 +0000

    pib

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,49 +2,41 @@
 
 int main() {
     printf("Desafio de cartas\n");
-    
+
     char estado;
-    char codigo[20];
-    char cidade[30];
+    char codigo[50];
+    char cidade[50];
     int populacao;
     float area;
     float PIB;
-    int PontoTuristico;
+    int turismo;
 
     printf("Digite o estado: \n");
     scanf("%c", &estado);
-    printf("Estado: %c\n", estado);
-
-    printf("Digite o codigo da carta: \n");
-    scanf("%s", &codigo);
-    printf("Cidade: %s\n", codigo);
+    printf("O estado escolhido: %c\n", estado);
+    
+    printf("Digite o codigo: \n");
+    scanf("%s", codigo);
+    printf("O codigo da carta é: %s\n", codigo);
 
     printf("Digite a cidade: \n");
     scanf("%s", &cidade);
-    printf("Cidade: %s\n", cidade);
-
+    printf("A cidade é: %s\n", cidade);
+    
     printf("Digite a população: \n");
     scanf("%i", &populacao);
-    printf("A população é: %i\n", populacao);
+    printf("A população da cidade é: %i\n", populacao);
 
-    printf("Digite a area: \n");
+    printf("Digite a area da cidade: \n");
     scanf("%f", &area);
-    printf("A area é: %f\n", area);
+    printf("A area da cidade é: %f\n", area);
 
-    printf("Digite o PIB: \n");
+    printf("Digite o Produto Interno Bruno: \n");
     scanf("%f", &PIB);
-    printf("O Produto Interno Bruto é: %f\n", PIB);
+    printf("O produto interno Bruto é: %f\n", PIB);
+
+
     
-    printf("Digite a quantidade de pontos turiscos: \n");
-    scanf("%i", &PontoTuristico);
-    printf("A quantidade de pontos turisticos é: %i\n", PontoTuristico);
-
-    printf("Codigo: %s\n", codigo);
-    printf("Estado: %s - Cidade: %s\n", estado, cidade);
-    printf("Area: %f - População: %i\n", area, populacao);
-    printf("Pontos Turiscos: %i - Produto Interno Bruto: %f", PontoTuristico, PIB);
-
-
     return 0;
 
 }
